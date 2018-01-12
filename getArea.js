488b472b4de376c30b3c2edafe2a603d518e9e75
apello96 <apello@hotmail.it>
2018-01-12 11:14:47 +0100

    heroku app

---
+++ getArea.js
@@ -0,0 +1,13 @@
+const express = require('express'),
+const bodyParser = require('body-parser');
+const app = express();
+const getArea = require('./area');
+
+app.get('/',function(req,res) {
+	var xxx = getArea([parseInt(req.query.side1),parseInt(req.query.side2)]);
+	var area = {area : xxx}
+	if(xxx == -1)
+		res.status = 400;
+	res.json(area);
+})
+
